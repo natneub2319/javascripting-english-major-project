0f1ba995a761453d2152deb4f635fd9592f3ac11
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-02-25 09:57:47 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,11 +7,21 @@ if ( userInput === "burrito" ) {
 }
 */
 
-let userInput;
+/* let userInput;
 userInput = prompt("Who is your favorite music artist?", "Type your answer here.");
 if ( userInput === "Queen" ) {
   $("#response").html("We are the champions, my friend! You got great taste");
 } else {
   $("#response").html("Another one bites the dust");
 }
+*/
 
+let makeABurrito;
+makeABurrito = function(beansVariable){
+  let beansResponse;
+  beansResponse = "You ordered" + beansVariable + " beans. Good choice!";
+  $("#response").html(beansResponse);
+};
+  let blackBeans;
+  blackBeans = "black";
+  makeABurrito(blackBeans);
