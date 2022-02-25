b644dd092528c6cebc9201e363a07a67be596572
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-02-25 09:29:37 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-let userInput;
+/* let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
+}
+*/
+
+let userInput;
+userInput = prompt("Who is your favorite music artist?", "Type your answer here.");
+if ( userInput === "Queen" ) {
+  $("#response").html("Don't stop me now! You got great taste");
+} else {
+  $("#response").html("Another one bites the dust");
 }
