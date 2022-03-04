8b2ebd6512d35b7967d88e3ce3bc8e23abccad82
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-03-04 09:35:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,8 +27,18 @@ makeABurrito = function(beansVariable){
   makeABurrito(blackBeans);
 */ 
 
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
+/* let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a", "b", "c"];
 arrayOfNumbers = [1, 2, 3];
 arrayMixed = ["a", null, true, arrayOfNumbers, [4.5, 5.6]];
 $("#response").html(arrayOfStrings[2]);
+*/
+
+let myBurritoObject;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: 3
+  };
+$("#response").html(myBurritoObject["tortilla"]);
