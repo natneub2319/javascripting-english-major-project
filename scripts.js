96e0895df6c647a99d63b8dbc5dee60daa845385
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-03-04 09:24:32 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,3 +26,9 @@ makeABurrito = function(beansVariable){
   blackBeans = "black";
   makeABurrito(blackBeans);
 */ 
+
+let arrayOfStrings, arrayOfNumbers, arrayMixed;
+arrayOfStrings = ["a", "b", "c"];
+arrayOfNumbers = [1, 2, 3];
+arrayMixed = ["a", null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayOfStrings[2]);
