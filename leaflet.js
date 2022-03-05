172f5af4e0320646af6bfcf2f1d173b3c442b942
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-03-05 16:05:26 -0500

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -15,6 +15,25 @@ bobstLibrary.bindPopup("This is Bobst Library.");
 let bobstCoords;
 bobstCoords = L.latLng(40.729444, -73.997222);
 firstMap.panTo(bobstCoords);
-let eifelTower;
+/* let eifelTower;
 eiffelTower = L. latLng(48.858222, 2.2945);
 firstMap.panTo(eiffelTower);
+*/
+
+let washingtonSquarePark, washingtonSquareParkMarker;
+let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+washingtonSquarePark = L.latLng(40.730833, -73.9975);
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
+tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
+                 [40.7309, -73.9974], [40.7309, -73.9976]];
+tenThousandthPolygon = L.polygon(tenThousandth, {
+                                  color: "#268bd2",
+                                  fillColor: "#fdf6e3"
+                                }
+                        ).addTo(firstMap);
+thousandth = [[washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng + 0.001],
+              [washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng - 0.001],
+              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng - 0.001],
+              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng + 0.001]];
+thousandthPolyline = L.polyline(thousandth, {color: "#d33682"}
+                      ).addTo(firstMap);
