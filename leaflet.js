ee3374ca27f66f71c1b01f7ad151e4385afbb2a3
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-03-25 09:21:07 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -45,3 +45,10 @@ circle = L.circle(washingtonSquarePark, {radius: 100,
                                          opacity: 0.9,
                                          fillOpacity: 0.25}
           ).addTo(firstMap);
+let circle;
+circle = L.circle(bobstLibrary), {radius: 100,
+                                         color: "#859900",
+                                         fillColor: "#cb4b16",
+                                         opacity: 0.9,
+                                         fillOpacity: 0.25}
+          ).addTo(firstMap);
