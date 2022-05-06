11eb3f7048dc1672e4fc75631b2a0c0f53a0bc5c
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-05-06 14:23:42 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -9,34 +9,8 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
 
-/*
-let eifelTower;
-eiffelTower = L. latLng(48.858222, 2.2945);
-firstMap.panTo(eiffelTower);
+let viniCucinaBlasi, viniCucinaBlasiMarker, ilGiordinaAlFori, ilGiordinaAlForiMarker, baccanale, baccanaleMarker;
 
-let washingtonSquarePark, washingtonSquareParkMarker;
-let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
-washingtonSquarePark = L.latLng(40.730833, -73.9975);
-washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
-tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
-                 [40.7309, -73.9974], [40.7309, -73.9976]];
-tenThousandthPolygon = L.polygon(tenThousandth, {
-                                  color: "#268bd2",
-                                  fillColor: "#fdf6e3"
-                                }
-                        ).addTo(firstMap);
-thousandth = [[washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng + 0.001],
-              [washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng - 0.001],
-              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng - 0.001],
-              [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng + 0.001]];
-thousandthPolyline = L.polyline(thousandth, {color: "#d33682"}
-                      ).addTo(firstMap);
-*/
-let washingtonSquarePark, washingtonSquareParkMarker, bobstLibrary, bobstLibraryMarker, viniCucinaBlasi, viniCucinaBlasiMarker, ilGiordinaAlFori, ilGiordinaAlForiMarker, baccanale, baccanaleMarker;
-washingtonSquarePark = L.latLng(40.730833, -73.9975);
-washingtonSquareParkMarker = L.marker(washingtonSquarePark).bindPopup("This is Washington Square Park").addTo(firstMap);
-bobstLibrary = L.latLng(40.729444, -73.997222);
-bobstLibraryMarker = L.marker(bobstLibrary).bindPopup("This is Bobst Library").addTo(firstMap);
 viniCucinaBlasi = L.latLng(41.896783, 12.476332);
 viniCucinaBlasiMarker = L.marker(viniCusinaBlasi).bindPopup("This is Vini & Cucina Blasi").addTo(firstMap);
 ilGiordinaAlFori = L.latLng(41.893233, 12.488525);
@@ -44,13 +18,13 @@ ilGiordinaAlFori = L.marker(ilGiordinaAlFori).bindPopup("This is il Giordana al 
 baccanale = L.latLng(41.895996, 12.472184);
 baccanaleMarker = L.marker(baccanale).bindPopup("This is Baccanale").addTo(firstMap);
 let circle;
-circle = L.circle(washingtonSquarePark, {radius: 100,
+circle = L.circle(viniCucinaBlasi,       {radius: 100,
                                          color: "#859900",
                                          fillColor: "#cb4b16",
                                          opacity: 0.9,
                                          fillOpacity: 0.25}
           ).addTo(firstMap);
-circle = L.circle(bobstLibrary),        {radius: 50,
+circle = L.circle(ilGiordinaAlFori),     {radius: 50,
                                          color: "#859900",
                                          fillColor: "#cb4b16",
                                          opacity: 0.9,
