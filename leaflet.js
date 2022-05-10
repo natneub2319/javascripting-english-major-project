9fe16b2b2686edbed8d0a00b9e5f963a22b41d3d
natneub2319 <99975606+natneub2319@users.noreply.github.com>
2022-05-10 16:27:16 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -7,7 +7,7 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
       maxZoom: 18
     });
 tileLayer.addTo(firstMap);
-firstMap.setView([48.6, 10.3], 10);
+firstMap.setView([32.6, 8.3], 10);
 let buddyRoma, buddyRomaMarker, pastaVino, pastaVinoMarker, baGhetto, baGhettoMarker;
 buddyRoma = L.latLng(41.8966557, 12.4745354);
 buddyRomaMarker = L.marker(buddyRoma).bindPopup("This is Buddy Roma, a vegan/vegetarian restaurant.").addTo(firstMap);
